d27f326c13e0d3f98cbb8b7f9882dc366ee6e439
Diego Luis Auza Trujillo <diegoluisauzatrujillo@Diegos-MacBook-Pro.local>
2025-03-31 19:54:12 -0500

    Adding user buttons

--- src/utils/Data/Data.jsx
+++ src/utils/Data/Data.jsx
@@ -6,10 +6,28 @@ const Data = [
     name: "Ausal Ganadera 0.5%",
     id: "05",
     filename: `AUSAL_GANADERA_05%_${currentDate}`,
+    color: "" ,
     data: new Batch(
       "AUSAL GANADERA 0.5%",
       "123ffff45",
-      "50kg",
+      "40kg",
+      `AU SAL GANADERA 0.5%: SAL MINERALIZADA PARA GANADO DE CARNE EN SUS ETAPAS 
+        DE LEVANTE Y CEBA. NO SUMINISTRAR EN ZONAS SELENÍFERAS.`,
+      `Cloruro de sodio, Carbonato de Calcio, Flor de Azufre, Fosfato Monobicalcico y/o
+        Bicalcico y/o Tricalcico, Melaza de Caña y/o Glicerol, Óxido de Magnesio y/o Sulfato de
+        Magnesio, Óxido de Zinc y/o Sulfato de Zinc, Sulfato de Cobre, y/o Cobre Tribasico,
+        Selenio de Sodio, Yoduro de Potasio, Carbonato de Cobalto.`,
+      8
+    ),
+  },
+  {
+    name: "Ausal Ganadera 8%",
+    id: "8",
+    filename: `AUSAL_GANADERA_8%_${currentDate}`,
+    data: new Batch(
+      "AUSAL GANADERA 8%",
+      "123ffff45",
+      "40kg",
       `AU SAL GANADERA 0.5%: SAL MINERALIZADA PARA GANADO DE CARNE EN SUS ETAPAS 
         DE LEVANTE Y CEBA. NO SUMINISTRAR EN ZONAS SELENÍFERAS.`,
       `Cloruro de sodio, Carbonato de Calcio, Flor de Azufre, Fosfato Monobicalcico y/o
